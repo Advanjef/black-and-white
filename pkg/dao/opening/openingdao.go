c0ff45d75094e82bc7d74ee69721ee2e7793c832
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2019-07-01 23:44:35 +0900

    [fix]warningを消すため、docを記載

--- pkg/dao/opening/openingdao.go
+++ pkg/dao/opening/openingdao.go
@@ -6,16 +6,17 @@ import (
 	"github.com/hunhunyosshy/black-and-white/pkg/db"
 )
 
+// Opening is struct which stores how to fight about black-and-white.
 type Opening struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
+// FetchIndex gets list of opening.
 func FetchIndex() []Opening {
 	db := db.Connect()
 	defer db.Close()
 
-	//rowを取得
 	rows, err := db.Query("SELECT * FROM opening")
 	if err != nil {
 		log.Fatal(err.Error())
@@ -32,6 +33,7 @@ func FetchIndex() []Opening {
 	return openingArgs
 }
 
+// FetchByKey gets opening by id.
 func FetchByKey(id string) []Opening {
 	db := db.Connect()
 	defer db.Close()
